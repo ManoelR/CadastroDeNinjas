fd50382e35177f6a6b5cc9500748346566a0d031
Manoel Ribeiro <manoelarque@gmail.com>
2026-05-21 08:23:05 -0300

    feat: cria as rotas do MissoesController com mock

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,29 +1,29 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("missoes")
+@RestController // É uma rota para as API's
+@RequestMapping("missoes") // Mapeia as API's
 public class MissoesController {
 
-    // GET -- Mandar uma requisição para mostrar as missões
+    // GET -- Manda uma requisição para mostrar as missões
     @GetMapping("/listar")
     public String listarMissoes() {
         return "Missões listadas com sucesso";
     }
 
-    // POST -- Mandar uma requisição para criar as missões
+    // POST -- Usuário manda uma requisição para criar as missões
     @PostMapping("/criar")
     public String criarMissao() {
         return "Missão criada com sucesso";
     }
 
-    // PUT -- Mandar uma requisição para alterar as missões
+    // PUT -- Manda uma requisição para alterar as missões
     @PutMapping("/alterar")
     public String alterarMissao() {
         return "Missão alterada com sucesso";
     }
 
-    // DELETE -- Mandar uma requisição para deletar as missões
+    // DELETE -- Manda uma requisição para deletar a missão
     @DeleteMapping("/Deletar")
     public String deletarMissao() {
         return "Missão deletada com sucesso";
